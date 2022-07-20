c9580514cac6518ed1dfed35413cb6eb79266cb9
Victor Hugo <victorhdoliveira@gmail.com>
2022-07-20 19:51:42 -0300

    Até o ex6

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,94 @@
+//1
+
+//const a = 5;
+//const b = 15;
+//const c = 50;
+
+//console.log(a + b);
+//console.log(a - b);
+//console.log(a * b);
+//console.log(a / b);
+//console.log(a % b);
+
+//2
+
+//if (a > b) {
+   //console.log (a);
+//}
+//else if (a < b){
+    //console.log (b);
+   //}
+//else {
+    //console.log("Valores Iguais");
+//}
+
+//3
+
+//if (a > b && a > c) {
+    //console.log (a);
+// }
+ //else if (b > a && b > c){
+     //console.log (b);
+    //}
+//else if (c > a && c > b){
+    //console.log (c);
+    //}
+ //else {
+     //console.log("Não encontrado");
+ //}
+
+ //4
+
+ //if (a > 0) {
+    //console.log("positive");
+ //}
+ //else if (a < 0){
+        //console.log("negative");
+    //}
+ //else { 
+    //console.log("zero");
+//} 
+
+//5
+
+//const ang1 = 60;
+//const ang2 = 60;
+//const ang3 = 60;
+//const angSoma = ang1 + ang2 + ang3;
+
+//if (ang1 <= 0 || ang2 <= 0 || ang3 <= 0) {
+    //console.log("Erro: Ângulo Inválido")
+//}
+//else if (angSoma !== 180) {
+    //console.log("false");
+//}
+//else { (angSoma == 180) 
+    //console.log("true");
+//}
+
+//6
+//let peça = "CAVALO";
+
+//switch (peça.toLowerCase()){
+    //case "rei":
+        //console.log("Uma casa em qualquer direção");
+        //break;
+    //case "dama":
+        //console.log("Qualquer direção");
+        //break;
+    //case "torre":
+        //console.log("Vertical e horizontal");
+        //break;    
+    //case "bispo":  
+        //console.log("Diagonal");
+        //break;
+    //case "cavalo":
+        //console.log("Em L");
+        //break;
+    //case "peão":
+        //console.log ("Em frente");
+        //break;
+    //default:
+        //console.log ("Peça não identificada")    
+//}
+
