c4c560c60d3e821ac68b053376de228699dc97bf
Victor Hugo <victorhdoliveira@gmail.com>
2022-07-21 19:10:40 -0300

    finalizando os exercícios regulares de Array e Loop

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script4_2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script4_2.js
@@ -2,14 +2,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //1
 
-for (let index = 0; index < numbers.length; index++) {
+for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
 
 //2
 
 let sum = 0;
-for (let index=0; index < numbers.length; index++) {
+for (let index=0; index < numbers.length; index += 1) {
     sum += numbers[index];
 }
 console.log(sum);
@@ -29,23 +29,51 @@ else {
 } 
 
 //5
-let maior = Math.max.apply(numbers)
-for (let index=0; index < numbers.length; index++) {
-    if (numbers[index] > maior); {
-        maior = numbers[index];
-    } 
-}
-console.log(maior)
+//console.log(Math.max.apply(null, numbers));
 
+let maiorN = numbers[0];
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorN) {
+      maiorN = numbers[index];
+    }
+  }
+  
+  console.log(maiorN);
+  
 //6
-let par = 0;
+
 let impar = 0;
 
-for (let index=0; index < numbers.length; index++) {
-    if (numbers[index] % 2 != 0);
-
-
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0); {
+        impar += 1;
     }
-//else {
-    //console.log ('nenhum valor ímpar encontrado');
-//}
+}
+    if (impar === 0) {
+        console.log ('nenhum valor ímpar encontrado')
+    } else {
+    console.log (impar);
+}
+
+//7
+
+let menorN = numbers[0];
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] < menorN) {
+      menorN = numbers[index];
+    }
+  }
+  console.log(menorN);
+  
+//8
+
+let lista = []
+for (let index = 1; index < 26; index +=1){
+    lista.push(index);
+}
+console.log (lista);
+
+//9
+let div2 = 1/2
+for (let index = 0; index < lista.length; index += 1) {
+    lista[index]*div2 }
