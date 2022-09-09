8cf01fa87e2bbc7d06c91b7b2089f14a9c8c53b9
Victor Hugo <victorhdoliveira@gmail.com>
2022-09-09 20:48:17 -0300

    Exercícios do dia 9.3

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-3-Jest-testes-assincronos/magic.test.js
@@ -0,0 +1,29 @@
+const { getMagicCard } = require('../magic');
+require('../../startest/mockSetup');
+
+jest.setTimeout(10000);
+
+describe('1 - Testes da função getMagicCard', () => {
+  it('1.1 Verifique se getMagicCard é uma função.', () => {
+    expect(typeof getMagicCard).toBe('function');
+  });
+
+  it('1.2 Verifique se a função fetch foi chamada.', async () => {
+    await getMagicCard('130550');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('1.3 Verifique se a função fetch foi chamada com o endpoint correto', async () => {
+    const expectUrl = 'https://api.magicthegathering.io/v1/cards/130550';
+    await getMagicCard('130550');
+    expect(fetch).toBeCalledWith(expectUrl);
+  });
+});
+
+describe('2 - Testando o retorno da função getMagicCard', () => {
+  it('2.1 Verifique se a carta retornada possui o nome Ancestor\'s Chosen.', async () => {
+    const response = await getMagicCard('130550');
+    const { name } = response;
+    expect(name).toBe('Ancestor\'s Chosen');
+  });
+});
